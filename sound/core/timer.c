5fdc103b9733b4217a315be41d3ca945143eeca6
Takashi Iwai <tiwai@suse.de>
2017-06-02 15:03:38 +0200

    BACKPORT: ALSA: timer: Fix race between read and ioctl
    The read from ALSA timer device, the function snd_timer_user_tread(),
    may access to an uninitialized struct snd_timer_user fields when the
    read is concurrently performed while the ioctl like
    snd_timer_user_tselect() is invoked.  We have already fixed the races
    among ioctls via a mutex, but we seem to have forgotten the race
    between read vs ioctl.
    
    This patch simply applies (more exactly extends the already applied
    range of) tu->ioctl_lock in snd_timer_user_tread() for closing the
    race window.
    
    Reported-by: Alexander Potapenko <glider@google.com>
    Tested-by: Alexander Potapenko <glider@google.com>
    Cc: <stable@vger.kernel.org>
    Signed-off-by: Takashi Iwai <tiwai@suse.de>
    (cherry picked from commit d11662f4f798b50d8c8743f433842c3e40fe3378)
    Signed-off-by: Connor O'Brien <connoro@google.com>
    Bug: 62201221
    
    Change-Id: I67a3b5153c39ce9f6d7571b5aa8faabe5e3dbb83

--- sound/core/timer.c
+++ sound/core/timer.c
@@ -1922,6 +1922,7 @@ static ssize_t snd_timer_user_read(struct file *file, char __user *buffer,
 	}
 	spin_unlock_irq(&tu->qlock);
  _error:
+	mutex_unlock(&tu->ioctl_lock);
 	return result > 0 ? result : err;
 }
 
